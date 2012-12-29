fe4b5645bbf8030797da00ead5262ddaa1bcedb8
Bryant <bwmairs@ucsc.edu>
2012-12-29 17:23:26 -0500

    Ran util.py through pep8.py

--- gerbmerge/util.py
+++ gerbmerge/util.py
@@ -11,16 +11,20 @@ Rugged Circuits LLC
 http://ruggedcircuits.com/gerbmerge
 """
 
+
 def in2gerb(value):
     """Convert inches to 2.5 Gerber units"""
-    return int(round(value*1e5))
+    return int(round(value * 1e5))
+
 
 def gerb2in(value):
     """Convert 2.5 Gerber units to inches"""
-    return float(value)*1e-5
+    return float(value) * 1e-5
+
 
 def mil2gerb(value):
-    return in2gerb(float(value)/1000.0)
+    return in2gerb(float(value) / 1000.0)
+
 
 def in2mil(value):
     return float(value) * 1000.0
